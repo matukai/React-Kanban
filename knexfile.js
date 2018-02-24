3473e748a76c9dcb740b98570f1980388bb635fc
matukai <matt.tanji@gmail.com>
2018-02-23 16:48:37 -1000

    boilerplate setup

---
+++ knexfile.js
@@ -0,0 +1,55 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'kanban',
+      password : 'password',
+      database : 'react_kanban',
+      charset: 'utf8'
+    },
+    migrations: {
+      directory: path.join(__dirname + '/knex/migrations'),
+    },
+    seeds: {
+      directory: path.join(__dirname + '/knex/seeds')
+    },
+    debug: true
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
