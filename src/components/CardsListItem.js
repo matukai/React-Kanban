e4642f22df43833bf3e169b7aefe8df94b85df04
matukai <matt.tanji@gmail.com>
2018-02-26 10:34:52 -1000

    cards rendering priority render fixed

--- src/components/CardsListItem.js
+++ src/components/CardsListItem.js
@@ -7,7 +7,7 @@ export const CardsListItem = (props) => {
         <div className="card">
 
           <div className="attribute"><span>Title: </span>{props.title}</div>
-          <div className="attribute"><span>Priority: </span>{props.Priority}</div>
+          <div className="attribute"><span>Priority: </span>{props.priority}</div>
           <div className="attribute"><span>Status: </span>{props.status}</div>
           <div className="attribute"><span>Created By: </span>{props.created_by}</div>
           <div className="attribute"><span>Assigned To: </span>{props.assigned_to}</div>
