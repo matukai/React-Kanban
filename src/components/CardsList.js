bdb639bf5284fffe717d7815b57a28cf6ab081fa
matukai <matt.tanji@gmail.com>
2018-02-27 15:02:02 -1000

    CardsList refactored to skeleton smart component, functionality still working

--- src/components/CardsList.js
+++ src/components/CardsList.js
@@ -1,27 +1,56 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardsListItem from './CardsListItem';
+import {connect} from 'react-redux';
 
 
-export default (props) => {
-  let cards = props.cards;
-  let filter = props.filter;
-  let status = props.status
-  //console.log('CARDS LIST',cards)
+class CardList extends Component {
+  constructor(props) {
+    super(props)
+    let cards = props.cards;
+    let filter = props.filter;
+    let status = props.status;
+  }
 
-return (
-  <div className={status}>
-    {cards.filter((element)=> {
-      return element.status === filter
-    }).map((element,idx) => {
-      return (
-        <CardsListItem
-        key={idx}
-        title={element.title}
-        priority={element.priority}
-        status={element.status}
-        created_by={element.created_by}
-        asssigned_to={element.asssigned_to}
-        />)})}
-  </div>)
+
+
+
+
+
+
+
+  render() {
+    return (
+      <div className={this.props.status}>
+        {this.props.cards.filter((element)=> {
+          return element.status === this.props.filter
+        }).map((element,idx) => {
+          return (
+            <CardsListItem
+            key={idx}
+            title={element.title}
+            priority={element.priority}
+            status={element.status}
+            created_by={element.created_by}
+            asssigned_to={element.asssigned_to}
+            />)})}
+      </div>
+    );
+  }
 }
 
+const mapStateToProps = state => {
+  return {
+
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    
+  }
+}
+
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(CardList)
